e65198beb3a4f5abe7f77bbb749cff89c9642a71
Mithilesh <mithilesh042006@gmail.com>
2025-05-10 15:57:44 +0530

    init

--- server/settings.py
+++ server/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
